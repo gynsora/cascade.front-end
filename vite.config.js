2813ed0e9c330425a31e9a8ffb9bed311c79ca65
Gynsora <djamaninadjim@gmail.com>
2024-05-10 08:50:09 +0200

    deploy package.json v2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cascade.ecam-epmi.fr/",
+  base: "/cascade.front-end/",
 })
