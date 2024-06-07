48b68eae728cc0bf6359253b6e0363909d48bcc9
Gynsora <djamaninadjim@gmail.com>
2024-06-07 13:38:23 +0200

    gestion du drag and drop tablette et ordinateur

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { WrappedApp } from './App.jsx'
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from 'react-dnd-touch-backend'; //?
 import './index.css'
+
+const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0; //?
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <DndProvider backend={HTML5Backend}>
+    {/* <DndProvider backend={HTML5Backend}> */}
+    <DndProvider backend={isTouchDevice ? TouchBackend : HTML5Backend}>
     <WrappedApp />
     </DndProvider>
   </React.StrictMode>,
